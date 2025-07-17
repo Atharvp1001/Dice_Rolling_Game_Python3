4fd4aa1bacd053e153f826219afd95e7b70c4363
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-07-17 17:03:46 +0530

    Done

---
+++ dice_rolling_game.py
@@ -0,0 +1,20 @@
+import random
+
+while True:
+    user_input = input("Roll the dice? (y/n): ").lower()
+
+    if user_input == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f"({die1}, {die2})")
+
+    elif user_input == 'n':
+        print("Thanks for Playing")
+        break
+    
+    else:
+        print("Invalid Input")
+
+
+
+
